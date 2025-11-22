73b15c0d07357fd77aaea60407971f8e8cb8c73e
Martin Karrer <mkarrer@amazon.de>
2025-11-22 17:33:05 +0100

    Initial commit: Mayfly landing page

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"], theme: {
+    extend: {
+      fontFamily: {
+        ptsans: ['PTSans', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        ptsans: ['PTSans', 'sans-serif'],
+      },
+    },
+  },
+}
+
